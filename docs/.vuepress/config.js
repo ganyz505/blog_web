51c09b33e403848d802c4c09f78d39bf00d66461
ganyzpro <894125348@qq.com>
2021-02-22 00:07:42 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     themeConfig: {
       // sidebarDepth: 4, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-      lastUpdated: 'Last Updated' ,// 文档更新时间：每个文件git最后提交的时间,
+        lastUpdated: 'Last Updated' ,// 文档更新时间：每个文件git最后提交的时间,
         nav:[
           {text: 'Web', link: '/web/'},
           {text: '进阶', link: '/other/'},
